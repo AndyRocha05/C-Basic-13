30e62f3e88ece70bd3398d040b2980cff3819387
Andy Rocha <75402294+AndyRocha05@users.noreply.github.com>
2021-04-19 11:39:13 -0700

    Delete mainapp directory

--- mainapp/forms.py
+++
@@ -1,9 +0,0 @@
-# forms.py 
-from django import forms 
-from mainapp.models import Image
-  
-class ImgForm(forms.ModelForm): 
-  
-    class Meta: 
-        model = Image 
-        fields = ['name', 'img', 'product'] 
